a58a7ebc417ea82a57696c2352cabd3a90f334b4
dhanuj kanchan <dhanujkanchan123@gmail.com>
2024-01-14 22:15:39 +0530

    added images and heroPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import HeroPage from "./components/heroPage/heroPage";
 import NavBar from "./components/navBar/navBar";
 
 function App() {
   return (
     <>
       <NavBar />
+      <HeroPage />
     </>
   );
 }
---
+++ src/components/heroPage/heroPage.tsx
@@ -0,0 +1,18 @@
+import { Grid } from "@mui/material";
+import "./heroPage.css";
+const HeroPage = () => {
+  return (
+    <>
+      <div className="hero-page">
+        <Grid container={true}>
+          <Grid item={true} xs={6}></Grid>
+
+          <Grid item={true} xs={6}>
+            <div className="hero-image"></div>
+          </Grid>
+        </Grid>
+      </div>
+    </>
+  );
+};
+export default HeroPage;
