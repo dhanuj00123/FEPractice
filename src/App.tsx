18beb0addeac28a6ee5cda670e7df0b9c87c478f
dhanuj kanchan <dhanujkanchan123@gmail.com>
2024-01-21 11:45:27 +0530

    added context api

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
 import "./App.css";
-import { Suspense, lazy } from "react";
+import { Suspense, lazy, useState } from "react";
 // import  Dashboard  from "./components/Dashboard/dashboard";
 import HeroPage from "./components/heroPage/heroPage";
 import NavBar from "./components/navBar/navBar";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { CountContext } from "./context";
 const Dashboard = lazy(() => import("./components/Dashboard/dashboard"));
 function App() {
+  const [count, setCount] = useState<number>(0);
   return (
     <>
       <BrowserRouter>
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<HeroPage />} />
-          <Route
-            path="/dashboard"
-            element={
-              <Suspense fallback={"...loading"}>
-                <Dashboard />
-              </Suspense>
-            }
-          />
-        </Routes>
+        <CountContext.Provider value={count}>
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<HeroPage />} />
+            <Route
+              path="/dashboard"
+              element={
+                <Suspense fallback={"...loading"}>
+                  <Dashboard />
+                </Suspense>
+              }
+            />
+          </Routes>
+        </CountContext.Provider>
       </BrowserRouter>
     </>
   );
--- src/components/heroPage/heroPage.tsx
+++ src/components/heroPage/heroPage.tsx
@@ -1,11 +1,17 @@
 import { Grid } from "@mui/material";
 import "./heroPage.css";
+import { useContext } from "react";
+import { CountContext } from "../../context";
 const HeroPage = () => {
+  const count = useContext(CountContext);
+
   return (
     <>
       <div className="hero-page">
         <Grid container={true}>
-          <Grid item={true} xs={6}></Grid>
+          <Grid item={true} xs={6}>
+            <h1>{count}</h1>
+          </Grid>
           <Grid item={true} xs={6}>
             <div className="hero-image"></div>
           </Grid>
